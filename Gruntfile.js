5c20e69a21111eff30addc6769986e59613d08be
Helgi Gylfason <hgylfason@gmail.com>
2014-12-28 22:17:49 +0000

    Changes to be committed: 	modified:   Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -317,7 +317,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'https://github.com/hgylfason/hgylfason.github.io.git',
           branch: 'gh-pages',
           commit: true,
           push: true
